a87a7171dbe4066f9850c1534978a90e907e9a6d
Jayant Verma <iamvermajayant@gmail.com>
2022-04-23 18:08:30 +0530

    added responsiveness

--- src/Info.js
+++ src/Info.js
@@ -1,10 +1,15 @@
 import React from "react";
-import './App.css';
+import "./App.css";
 
 const Info = () => {
   return (
     <div className="Info-heading-wrapper">
-      <h1 style={{ marginTop: "5rem" }}>What is Pomodoro Technique ?</h1>
+      <h1
+        className="Info-heading"
+        style={{ marginTop: "5rem", textAlign: "center" }}
+      >
+        What is Pomodoro Technique ?
+      </h1>
       <p className="Info-wrapper">
         The Pomodoro Technique is created by Francesco Cirillo for a more
         productive way to work and study. The technique uses a timer to break
@@ -13,6 +18,20 @@ const Info = () => {
         word for 'tomato', after the tomato-shaped kitchen timer that Cirillo
         used as a university student
       </p>
+      <h1
+        className="Info-heading"
+        style={{ marginTop: "2.5rem", textAlign: "center" }}
+      >
+        How to use Pomodoro Timer ?
+      </h1>
+      <div className="info-list-wrapper">
+        <ol className="info-list">
+          <li>Add your Task</li>
+          <li>Set Your Time for work and break</li>
+          <li>Play the button</li>
+          <li>stay focused ! 😇</li>
+        </ol>
+      </div>
     </div>
   );
 };
